1c89ac4aa49c34378cb9206476f8dbeea27151e7
Kaan Ozkan <kaanozkan97@gmail.com>
2020-04-26 23:11:36 -0400

    lexing wip

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,27 +1,99 @@
 #include "lexer.hpp"
 #include "iostream"
 
-/* struct Lexer { */
-/*     int pos; */
-/*     int line; */
-/*     Token* current_token; */
-/* }; */
-
-std::vector<Token> lex(const std::vector<char> input,
-                       std::vector<Token> &lexemes) {
-    bool encountered_error = false;
-
-    int line = 1;    // line number
-    int start = 0;   // first character in CURRENT lexeme
-    int current = 0; // character currently being looked at
-    while (current < input.size()) {
-        start = current;
-        // scan tokens
-
-        current++; // TODO
+// Add a token to tokens vector. Text is determined by start/current indices
+void add_token(Lexer* l, TokenType type) {
+    std::string text;
+    for(int i = l->start; i < l->current+1; i++) {
+        text += l->input.at(i);
     }
 
-    return lexemes;
+    Token token = {};
+    token.type = type;
+    token.text = text;
+    token.start_pos = l->start;
+    token.line = l->line;
+    l->tokens.push_back(token);
+}
+
+// Consumes token and cleanup
+void consume(Lexer* l) {
+    Token token = l->tokens.back();
+    token.end_pos = l->current;
+    l->current = 0;
+    l->start = 0;
+}
+
+std::vector<Token> lex(const std::vector<char> input,
+                       std::vector<Token> &tokens) {
+    bool encountered_error = false;
+
+    Lexer l = {};
+    l.input = input;
+    l.tokens = tokens;
+    l.line = 1;    // line number
+    l.start = 0;   // index of first character in CURRENT lexeme
+    l.current = 0; // index of character currently being looked at
+    while (l.current < input.size()) {
+        l.start = l.current;
+        char token = input.at(l.current);
+
+        switch(token) {
+            case '(':
+                add_token(&l, LEFT_PAREN);
+                consume(&l);
+                break;
+            case ')':
+                add_token(&l, RIGHT_PAREN);
+                consume(&l);
+                break;
+            case '{':
+                add_token(&l, LEFT_BRACE);
+                consume(&l);
+                break;
+            case '}':
+                add_token(&l, RIGHT_BRACE);
+                consume(&l);
+                break;
+            case ',':
+                add_token(&l, COMMA);
+                consume(&l);
+                break;
+            case '.':
+                add_token(&l, DOT);
+                consume(&l);
+                break;
+            case '-':
+                add_token(&l, MINUS);
+                consume(&l);
+                break;
+            case '+':
+                add_token(&l, PLUS);
+                consume(&l);
+                break;
+            case ';':
+                add_token(&l, SEMICOLON);
+                consume(&l);
+                break;
+            case '*':
+                add_token(&l, STAR);
+                consume(&l);
+                break;
+
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                l.line++;
+                break;
+            default:
+                encountered_error = true;
+                lexer_error(l.line);
+        }
+    }
+
+    return l.tokens;
 }
 
 // TODO: More specific error reporting
--- src/lexer.hpp
+++ src/lexer.hpp
@@ -10,7 +10,11 @@ enum TokenType {
     LEFT_BRACE,
     RIGHT_BRACE,
     COMMA,
+    DOT,
+    MINUS,
+    PLUS,
     SEMICOLON,
+    STAR,
     EQUAL,
     IDENTIFIER,
     STRING_LITERAL,
@@ -20,11 +24,22 @@ enum TokenType {
 
 struct Token {
     TokenType type;
+    std::string text;
     int start_pos;
     int end_pos;
-    std::string lexeme;
+    int line;
 };
 
-std::vector<Token> lex(std::vector<char> input, std::vector<Token> &lexemes);
+struct Lexer {
+    int line;
+    int current;
+    int start;
+    Token* current_token;
+    std::vector<char> input;
+    std::vector<Token> tokens;
+};
 
+void add_token(Lexer* l, TokenType type);
+void consume(Lexer* l);
+std::vector<Token> lex(std::vector<char> input, std::vector<Token> &lexemes);
 void lexer_error(int line);
